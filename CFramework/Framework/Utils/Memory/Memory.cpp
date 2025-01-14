bcc49c864f38a25fc80e826c2c0652a67d8d86f4
Neko64V <sawayump@gmail.com>
2025-01-14 18:17:50 +0900

    フレームワーク周りの更新

--- CFramework/Framework/Utils/Memory/Memory.cpp
+++ CFramework/Framework/Utils/Memory/Memory.cpp
@@ -1,75 +1,71 @@
 ﻿#include "Memory.h"
 
-bool Memory::AttachProcess(const std::string targetName, int mode)
+bool Memory::AttachProcess(const char* targetName, int InitMode)
 {
-    switch (mode)
+    // 任意の方法でPIDを取得する
+    if (InitMode == WINDOW_TITLE || InitMode == WINDOW_CLASS)
     {
-    case MODE::WINDOW: {
-        HWND TargetHwnd = FindWindowA(targetName.c_str(), nullptr); // ClassName
 
-        if (!TargetHwnd) {
-            MessageBoxA(nullptr, "Please open { GAME NAME #1 }", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK);
+        // ウィンドウベースでPIDを取得
+        HWND hWindow = InitMode == WINDOW_TITLE ? FindWindowA(nullptr, targetName) : FindWindowA(targetName, nullptr);
+
+        if (!hWindow) {
+            MessageBoxA(nullptr, "please open BF4", "Initialize Failed", MB_TOPMOST | MB_ICONERROR | MB_OK);
             return false;
         }
 
-        GetWindowThreadProcessId(TargetHwnd, &PID);
-        pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
-
-        if (!pHandle) {
-            MessageBoxA(nullptr, "Failed to get process handle", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK); // 管理者として実行するかアンチチートをバイパスする
-            return false;
-        }
-
-        char pModule[128]{};
-        GetModuleBaseNameA(m.pHandle, nullptr, pModule, sizeof(pModule));
-        g_BaseAddress = GetModuleBase(pModule);
-
-        if (g_BaseAddress == 0)
-            MessageBoxA(nullptr, "Base is 0", "WARNING", MB_OK | MB_TOPMOST);
-
-        return true;
+        GetWindowThreadProcessId(hWindow, &m_dwPID);
     }
-    case MODE::PROCESS: {
+    else if (InitMode == PROCESS)
+    {
+        // 実行ファイル名ベースでPIDを取得
         PROCESSENTRY32 process = GetProcess(targetName);
 
         if (process.th32ProcessID == 0) {
-            MessageBoxA(nullptr, "Please open { GAME NAME #2 }", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK);
+            MessageBoxA(nullptr, "please open BF4", "Initialize Failed", MB_TOPMOST | MB_ICONERROR | MB_OK);
             return false;
         }
 
-        PID = process.th32ProcessID;
-        pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, process.th32ProcessID);
-
-        if (!pHandle) {
-            MessageBoxA(nullptr, "Failed to get process handle", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK); // 管理者として実行するかアンチチートをバイパスする
-            return false;
-        }
-
-        char pModule[128]{};
-        GetModuleBaseNameA(m.pHandle, nullptr, pModule, sizeof(pModule));
-        g_BaseAddress = GetModuleBase(pModule);
-
-        if (g_BaseAddress == 0)
-            MessageBoxA(nullptr, "Base is 0", "WARNING", MB_OK | MB_TOPMOST);
-
-        return true;
+        m_dwPID = process.th32ProcessID;
     }
-    default:
-        MessageBoxA(nullptr, "Invalid memory init option", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK);
+    else
+    {
+        // 無効なオプション
+        MessageBoxA(nullptr, "Invalid memory init option", "Initialize Failed", MB_TOPMOST | MB_ICONERROR | MB_OK);
         return false;
     }
+
+    // プロセスハンドルを取得
+    m_hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, m_dwPID);
+
+    if (!m_hProcess) {
+        MessageBoxA(nullptr, "Failed to get process handle", "Init Error", MB_TOPMOST | MB_ICONERROR | MB_OK); // 管理者として実行するかアンチチートをバイパスする
+        return false;
+    }
+
+    return true;
+}
+
+void Memory::GetBaseAddress(const char* targetName1, const char* targetName2)
+{
+    // ベースアドレスを取得
+    char pModule[128]{};
+    GetModuleBaseNameA(m_hProcess, nullptr, pModule, sizeof(pModule)); // like "FiveM_b2944_GTAProcess.exe"
+
+    m_gProcessBaseAddr = GetModuleBase(pModule);
+    m_gNameDllBaseAddr = GetModuleBase(targetName2);
 }
 
 void Memory::DetachProcess()
 {
-    CloseHandle(pHandle);
+    CloseHandle(m_hProcess);
 }
 
 uintptr_t Memory::GetModuleBase(const std::string moduleName)
 {
     MODULEENTRY32 entry{};
     entry.dwSize = sizeof(MODULEENTRY32);
-    const auto snapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, PID);
+    const auto snapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, m_dwPID);
 
     while (Module32Next(snapShot, &entry))
     {
--- CFramework/Framework/Utils/Memory/Memory.h
+++ CFramework/Framework/Utils/Memory/Memory.h
@@ -5,43 +5,50 @@
 #include <psapi.h>
 #include <string>
 
-enum MODE : int
+/*	[+] メモリやオーバーレイの初期化モードを設定します
+
+	WINDOW_TITLE : ウィンドウのタイトルを使用します
+	WINDOW_CLASS : ウィンドウのクラス名を使用します
+	PROCESS      : 実行ファイル名を使用します				*/
+enum InitializeMode : int
 {
-	WINDOW,
+	WINDOW_TITLE,
+	WINDOW_CLASS,
 	PROCESS
 };
 
+// これからのコーディングを少し楽にする素晴らしいMemoryクラス
 class Memory
 {
 private:
-	DWORD PID;
-
 	uintptr_t GetModuleBase(const std::string moduleName);
 	PROCESSENTRY32 GetProcess(const std::string processName);
 public:
-	HANDLE pHandle;
-	uintptr_t g_BaseAddress;
+	DWORD m_dwPID;
+	HANDLE m_hProcess;
+	uintptr_t m_gProcessBaseAddr;
+	uintptr_t m_gNameDllBaseAddr;
 
-	bool AttachProcess(const std::string targetName, int mode);
+	bool AttachProcess(const char* targetName, int InitMode);
+	void GetBaseAddress(const char* targetName1, const char* targetName2);
 	void DetachProcess();
 
 	template <typename T>
 	constexpr const T Read(const uintptr_t& address) const noexcept
 	{
 		T value{};
-		ReadProcessMemory(pHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
+		ReadProcessMemory(m_hProcess, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
 		return value;
 	}
-
 	template <typename T>
 	constexpr void Write(const uintptr_t& address, const T& value) const noexcept
 	{
-		WriteProcessMemory(pHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
+		WriteProcessMemory(m_hProcess, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
 	}
 	bool ReadString(uintptr_t address, LPVOID buffer, SIZE_T size) const
 	{
 		SIZE_T size_read;
-		return !!::ReadProcessMemory(pHandle, LPCVOID(address), buffer, size, &size_read) && size_read > 0;
+		return !!::ReadProcessMemory(m_hProcess, LPCVOID(address), buffer, size, &size_read) && size_read > 0;
 	}
 };
 
